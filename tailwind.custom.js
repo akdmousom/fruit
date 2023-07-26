171daf189af1bb2ed4637a2b473f26ed1b7c05b2
akdmausom <contact.mousom@gmail.com>
2023-07-26 23:59:53 +0600

    fruit brust project is done

---
+++ tailwind.custom.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+          primary_color: '#F85559',
+          font_color: '#12121299',
+          title_color: '#121212',
+        }
+      }
+    }
+  }
